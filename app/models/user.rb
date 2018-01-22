61e1fcc8c5ddedf3424ddf27536411af0ae95704
Madhav Paudel <poudelmadhav143@gmail.com>
2018-01-23 00:02:26 +0545

    dependent added

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  has_many :ideas
+  has_many :ideas, dependent: :destroy
 end
